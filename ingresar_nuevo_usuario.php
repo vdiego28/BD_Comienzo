06c1608a02cf1e134ea563c3c985053a32d2c374
Gonzalo Concha <gonzalo.concha@uc.cl>
2020-10-21 15:38:05 -0300

    ingresar usuario vista

---
+++ ingresar_nuevo_usuario.php
@@ -0,0 +1,30 @@
+<?php include('templates/header.html'); ?>
+
+<body>
+<?php
+    require("config/conexion.php");
+
+    $nombre = $_POST["nombre_usuario"];
+    $edad = $_POST["edad_usuario"];
+    $pasaporte = $_POST["n_pasaporte_usuario"];
+    $nacionalidad = $_POST["nacionalidad_usuario"];
+    $contraseña = $_POST["contraseña_usuario"];
+
+    $query = "SELECT nombre FROM Usuarios WHERE nombre = '%nombre%'";
+    $result = $db -> prepare($query);
+    $result -> execute();
+    $nombres = $result -> fetchAll();
+
+    list($total) = mysql_fetch_row($result);
+    if ($total==0) {
+        <form align="center" action="index.php" method="post">
+            <br/></br>
+            <input type="submit" value="Ingresar">
+        </form>
+    }
+    if ($total!=0) or (strlen($contraseña)<1) or (strlen($nacionalidad)<1) or ($edad<1) or ($pasaporte<1) {
+        <form align="center" action="nueva_sesion.php" method="post">
+            <br/></br>
+            <input type="submit" value="Regresar">
+        </form>
+    }
---
+++ nueva_sesion.php
@@ -0,0 +1,27 @@
+<?php include('templates/header.html'); ?>
+
+<body>
+    <h1 align='center'> Crear Usuario </h1>
+    <p style="text-align:center";> Ingrese sus datos para crear un nuevo usuario. </p>
+
+    <br>
+
+    <form align="center" action="ingresar_nuevo_usuario.php" method="post">
+        Nombre:
+        <input type="text" name="nombre_usuario">
+        </br>
+        Edad:
+        <input type="number" name="edad_usuario">
+        <br/></br>
+        Numero de Pasaporte:
+        <input type="number" name="n_pasaporte_usuario">
+        <br/></br>
+        Nacionalidad:
+        <input type="text" name="nacionalidad_usuario">
+        <br/></br>
+        Contraseña:
+        <input type="text" name="contraseña_usuario">
+        <br/></br>
+        <input type="submit" value="Crear">
+    </form>
+    <br>
